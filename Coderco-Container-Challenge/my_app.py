2cee7aca22a4aba5720240e231fe5ac3a2868508
Abdi-Shakur457 <abdishakur457@hotmail.com>
2026-01-18 19:16:04 +0000

    add customisation of my web project

--- Coderco-Container-Challenge/my_app.py
+++ Coderco-Container-Challenge/my_app.py
@@ -1,29 +1,24 @@
-# my web flask application that runs on redis
-import os 
-from flask import Flask
+import os
+from flask import Flask, render_template
 from redis import Redis
+
 app = Flask(__name__)
 
-redis_host = os.getenv('LOCAL_HOST' , 'redis')
-redis_port = int(os.getenv('LOCAL_PORT' , 6379)) 
+redis_host = os.getenv("LOCAL_HOST", "redis")
+redis_port = int(os.getenv("LOCAL_PORT", 6379))
 
-r = Redis(host=redis_host , port=redis_port)
+r = Redis(host=redis_host, port=redis_port)
 
 @app.route("/")
-
 def index():
+    count = r.incr("visits")
+    return render_template("index.html", visit_count=count)
 
-    return " Welcome to my Coder co Container Web Application Docker Challange "
-
-@app.route('/count')
-
-def count():
-
-    count = r.incr('visits')
-
-    return f"You have visited this Page {count} times"
-
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5003)
 
-    app.run(host="0.0.0.0" , port=5003)
+
